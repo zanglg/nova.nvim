4f8c5fed73a4b3ac288215ac4e7ed2cb10158ba6
Zang Leigang <zang.lg@outlook.com>
2022-08-12 20:52:11 +0800

    setup empty nova color scheme

---
+++ lua/nova.lua
@@ -0,0 +1,15 @@
+local nova = {}
+
+nova.setup = function() end
+
+nova.load = function()
+	vim.api.nvim_command("highlight clear")
+	if vim.fn.exists("syntax_on") then
+		vim.api.nvim_command("syntax reset")
+	end
+
+	vim.o.termguicolors = true
+	vim.g.colors_name = "nova"
+end
+
+return nova
