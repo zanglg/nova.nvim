9acb52304730ef3141130a4c4be104b8f9ad16a9
Zang Leigang <zang.lg@outlook.com>
2022-09-08 00:27:28 +0800

    reduct 12 colors to 8, set default dark mode to palette from nova.app

--- lua/nova/groups/treesitter.lua
+++ lua/nova/groups/treesitter.lua
@@ -1,11 +1,11 @@
 local colors = require("nova").colors
 
 return {
-    rainbowcol1 = { fg = colors.blue },
-    rainbowcol2 = { fg = colors.red },
-    rainbowcol3 = { fg = colors.yellow },
-    rainbowcol4 = { fg = colors.violet },
+    rainbowcol1 = { fg = colors.red },
+    rainbowcol2 = { fg = colors.green },
+    rainbowcol3 = { fg = colors.blue },
+    rainbowcol4 = { fg = colors.yellow },
     rainbowcol5 = { fg = colors.purple },
-    rainbowcol6 = { fg = colors.green },
-    rainbowcol7 = { fg = colors.teal },
+    rainbowcol6 = { fg = colors.cyan },
+    rainbowcol7 = { fg = colors.violet },
 }
