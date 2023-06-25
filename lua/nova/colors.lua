e6ffa24d44cd181fb7c98397ceb0d02c82743c0a
Zang Leigang <zang.lg@outlook.com>
2023-06-25 21:43:24 +0800

    remove black theme

--- lua/nova/colors.lua
+++ lua/nova/colors.lua
@@ -3,7 +3,7 @@ local option = require("nova.option").option
 
 -- stylua: ignore
 local colors = {
-    nova = {
+    dark = {
         foreground    = hsl2rgb(225, 0.15, 0.75),
         comment       = hsl2rgb(225, 0.15, 0.50),
         inconspicuous = hsl2rgb(225, 0.15, 0.41),
@@ -44,32 +44,11 @@ local colors = {
         blue          = hsl2rgb(212, 0.80, 0.42),
         violet        = hsl2rgb(240, 0.50, 0.60),
         purple        = hsl2rgb(280, 0.60, 0.50),
-    },
-    dark = {
-        foreground    = hsl2rgb(235, 0.17, 0.75),
-        comment       = hsl2rgb(235, 0.17, 0.50),
-        inconspicuous = hsl2rgb(235, 0.17, 0.41),
-        splitline     = hsl2rgb(235, 0.17, 0.24),
-        selection     = hsl2rgb(235, 0.17, 0.21),
-        popupmenu     = hsl2rgb(235, 0.17, 0.18),
-        stripline     = hsl2rgb(235, 0.17, 0.15),
-        background    = hsl2rgb(235, 0.17, 0.12),
-
-        red           = hsl2rgb(345, 0.63, 0.63),
-        brown         = hsl2rgb(015, 0.20, 0.50),
-        yellow        = hsl2rgb(037, 0.60, 0.60),
-        lime          = hsl2rgb(067, 0.50, 0.50),
-        green         = hsl2rgb(136, 0.37, 0.49),
-        teal          = hsl2rgb(181, 0.40, 0.38),
-        cyan          = hsl2rgb(190, 0.71, 0.50),
-        blue          = hsl2rgb(220, 0.80, 0.65),
-        violet        = hsl2rgb(255, 0.50, 0.65),
-        purple        = hsl2rgb(291, 0.65, 0.65),
     }
 }
 
 if colors[option.theme] == nil then
-    return colors.nova
+    return colors.dark
 end
 
 return colors[option.theme]
