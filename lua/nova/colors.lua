5b2d95732353ddbdfe9a0a503838d46b9b5af841
Zang Leigang <zang.lg@outlook.com>
2023-07-08 16:19:45 +0800

    tweak light theme

--- lua/nova/colors.lua
+++ lua/nova/colors.lua
@@ -25,9 +25,9 @@ local colors = {
         purple        = hsl2rgb(291, 0.65, 0.65),
     },
     light = {
-        foreground    = hsl2rgb(200, 0.05, 0.40),
-        comment       = hsl2rgb(200, 0.05, 0.65),
-        inconspicuous = hsl2rgb(200, 0.05, 0.70),
+        foreground    = hsl2rgb(200, 0.15, 0.35),
+        comment       = hsl2rgb(200, 0.15, 0.55),
+        inconspicuous = hsl2rgb(200, 0.15, 0.70),
         splitline     = hsl2rgb(200, 0.15, 0.80),
         selection     = hsl2rgb(200, 0.15, 0.85),
         popupmenu     = hsl2rgb(200, 0.15, 0.90),
@@ -35,15 +35,15 @@ local colors = {
         background    = hsl2rgb(200, 0.15, 0.99),
 
         red           = hsl2rgb(350, 0.66, 0.47),
-        brown         = hsl2rgb(015, 0.20, 0.50),
+        brown         = hsl2rgb(015, 0.15, 0.50),
         yellow        = hsl2rgb(045, 0.75, 0.40),
         lime          = hsl2rgb(066, 0.50, 0.40),
         green         = hsl2rgb(123, 0.46, 0.34),
-        teal          = hsl2rgb(155, 0.65, 0.35),
+        teal          = hsl2rgb(173, 0.95, 0.25),
         cyan          = hsl2rgb(187, 0.90, 0.30),
         blue          = hsl2rgb(212, 0.80, 0.42),
         violet        = hsl2rgb(240, 0.50, 0.60),
-        purple        = hsl2rgb(280, 0.60, 0.50),
+        purple        = hsl2rgb(301, 0.63, 0.50),
     }
 }
 
