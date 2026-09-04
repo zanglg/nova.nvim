b6df98aeaa1bcbef4c7d18811e56748f44829e51
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-04 17:04:51 +0800

    refactor: make color conversion side-effect free

--- lua/nova/utils.lua
+++ lua/nova/utils.lua
@@ -1,24 +1,36 @@
 local M = {}
 
--- stylua: ignore start
-M.hsl2rgb = function(h, s, l)
+local function round(value)
+    return math.floor(value + 0.5)
+end
+
+function M.hsl2rgb(h, s, l)
+    h = h % 360
+
     local c = (1 - math.abs(2 * l - 1)) * s
     local x = c * (1 - math.abs((h / 60) % 2 - 1))
     local m = l - c / 2
+    local r, g, b
 
-    if     (0   <= h and h < 60)  then r, g, b = c, x, 0
-    elseif (60  <= h and h < 120) then r, g, b = x, c, 0
-    elseif (120 <= h and h < 180) then r, g, b = 0, c, x
-    elseif (180 <= h and h < 240) then r, g, b = 0, x, c
-    elseif (240 <= h and h < 300) then r, g, b = x, 0, c
-    elseif (300 <= h and h < 360) then r, g, b = c, 0, x
+    if h < 60 then
+        r, g, b = c, x, 0
+    elseif h < 120 then
+        r, g, b = x, c, 0
+    elseif h < 180 then
+        r, g, b = 0, c, x
+    elseif h < 240 then
+        r, g, b = 0, x, c
+    elseif h < 300 then
+        r, g, b = x, 0, c
+    else
+        r, g, b = c, 0, x
     end
 
-    r, g, b = ((r + m) * 255), ((g + m) * 255), ((b + m) * 255)
+    r = round((r + m) * 255)
+    g = round((g + m) * 255)
+    b = round((b + m) * 255)
 
-    function round(float) return math.floor(float + .5) end
-
-    return string.format("#%02x%02x%02x", round(r), round(g), round(b))
+    return string.format("#%02x%02x%02x", r, g, b)
 end
 
 return M
