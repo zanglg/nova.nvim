52f69d944c13b816ab83ac5c18e02c0aa42dc177
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-04 17:03:06 +0800

    refactor: split syntax highlight groups

---
+++ lua/nova/highlights/syntax.lua
@@ -0,0 +1,46 @@
+return function(colors)
+    return {
+        Comment = { fg = colors.comment },
+
+        Constant = { fg = colors.yellow },
+        String = { fg = colors.green },
+        Character = { fg = colors.green },
+        Number = { fg = colors.yellow },
+        Boolean = { fg = colors.yellow },
+        Float = { fg = colors.yellow },
+
+        Identifier = { fg = colors.foreground },
+        Function = { fg = colors.blue },
+
+        Statement = { fg = colors.purple },
+        Conditional = { fg = colors.purple },
+        Repeat = { fg = colors.purple },
+        Label = { fg = colors.purple },
+        Operator = { fg = colors.teal },
+        Keyword = { fg = colors.purple },
+        Exception = { fg = colors.purple },
+
+        PreProc = { fg = colors.violet },
+        Include = { fg = colors.violet },
+        Define = { fg = colors.violet },
+        Macro = { fg = colors.brown },
+        PreCondit = { fg = colors.violet },
+
+        Type = { fg = colors.red },
+        StorageClass = { fg = colors.cyan },
+        Structure = { fg = colors.purple },
+        Typedef = { fg = colors.red },
+
+        Special = { fg = colors.brown },
+        SpecialChar = { fg = colors.brown },
+        Tag = { fg = colors.brown },
+        Delimiter = { fg = colors.foreground },
+        SpecialComment = { fg = colors.red },
+        Debug = { fg = colors.blue },
+
+        Underlined = { underline = true },
+        Ignore = { fg = colors.inconspicuous },
+        Error = { fg = colors.red },
+        Todo = { fg = colors.green },
+    }
+end
