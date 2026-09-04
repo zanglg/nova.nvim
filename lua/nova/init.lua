9e687a3bde073076fb4d144504fc8b95f7c6a3d7
Zang Leigang <zanglg@users.noreply.github.com>
2026-09-04 17:05:03 +0800

    refactor: modernize colorscheme loading

--- lua/nova/init.lua
+++ lua/nova/init.lua
@@ -1,23 +1,26 @@
 local M = {}
 
-M.setup = function(opts)
-    -- override default options
+function M.setup(opts)
     require("nova.option").setup(opts)
 end
 
-M.load = function()
-    -- remove highlighting for groups added
+function M.load()
+    local opts = require("nova.option").option
+
     vim.cmd("highlight clear")
+    if vim.fn.exists("syntax_on") == 1 then
+        vim.cmd("syntax reset")
+    end
 
     vim.o.termguicolors = true
     vim.g.colors_name = "nova"
 
-    local groups = require("nova.groups")
+    package.loaded["nova.colors"] = nil
+    local colors = require("nova.colors")
+    local groups = require("nova.groups")(colors, opts)
 
-    for _, group in ipairs(groups) do
-        for hl, col in pairs(group) do
-            vim.api.nvim_set_hl(0, hl, col)
-        end
+    for name, highlight in pairs(groups) do
+        vim.api.nvim_set_hl(0, name, highlight)
     end
 end
 
